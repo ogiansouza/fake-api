b9ad76373d61acf870487381ff617ea1f038fa1a
Gian Souza <giansouzafreitas@gmail.com>
2023-02-22 20:52:05 -0300

    fix cors

--- src/pages/api/candidaturas.ts
+++ src/pages/api/candidaturas.ts
@@ -1,9 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
 
-export default function handler(
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await runMiddleware(req, res, cors)
+
   if (req.method === 'POST') {
     res.status(200).json({
         message: 'candidatura recebida com sucesso'
--- src/pages/api/vagas.ts
+++ src/pages/api/vagas.ts
@@ -1,10 +1,33 @@
 import vagas from '@/mocks/vagas'
 import Vaga from '@/models/Vaga'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
 
-export default function handler(
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Vaga[]>
 ) {
+  await runMiddleware(req, res, cors)
+
   res.status(200).json(vagas)
 }
