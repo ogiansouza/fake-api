b727059dffd1350fb00841e90de6ace5c37ee397
Gian Souza <giansouzafreitas@gmail.com>
2023-04-24 01:22:02 -0300

    validate payload

--- src/pages/api/efood/checkout.ts
+++ src/pages/api/efood/checkout.ts
@@ -21,10 +21,81 @@ function runMiddleware(
   })
 }
 
+function validate(body: any, res: any) {
+  if (!body) {
+    return false
+  } 
+
+  let isValid = true;
+  
+  try {
+    if (body.payment.card.active) {
+      isValid = body.payment.card.number.length > 0
+    }
+
+    if (body.products.length === 0) {
+      isValid = false
+    }
+  } catch(e) {
+    return false
+  }
+
+  return isValid
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  const randomId = parseInt((Math.random() * 100).toFixed(), 10);
+  const payload = {
+    products: [
+      {
+        id: 1,
+        price: 0,
+      }
+    ],
+    delivery: {
+      receiver: "string",
+      address: {
+        description: "string",
+        city: "string",
+        zipCode: "string",
+        number: 12,
+        complement: "string",
+      }
+    },
+    payment: {
+      card: {
+        name: "string",
+        number: "string",
+        code: 123,
+        expires: {
+          month: 12,
+          year: 1234,
+        },
+      },
+    },
+  };
   await runMiddleware(req, res, cors)
-  res.status(201)
+
+  if (req.method === 'GET') {
+    res.status(200).json({
+      payload,
+    })
+  } else if (req.method === 'POST') {
+    if (validate(req.body, res)) {
+      res.status(201).json({
+        orderId: `#256${randomId}`
+      })
+    } else {
+      res.status(400).json({
+        message: 'Reveja os dados enviados'
+      })
+    }
+  } else {
+    res.status(400).json({
+      message: 'Método não implementado'
+    })
+  }
 }
--- src/pages/api/eplay/checkout.ts
+++ src/pages/api/eplay/checkout.ts
@@ -21,10 +21,85 @@ function runMiddleware(
   })
 }
 
+function validate(body: any, res: any) {
+  if (!body) {
+    return false
+  } 
+
+  let isValid = true;
+  
+  try {
+    if (body.payment.card.active) {
+      isValid = body.payment.card.number.length > 0
+    }
+
+    if (body.products.length === 0) {
+      isValid = false
+    }
+  } catch(e) {
+    return false
+  }
+
+  return isValid
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  const randomId = parseInt((Math.random() * 10000).toFixed(), 10);
+  const payload = {
+    products: [
+      {
+        id: 1,
+        price: 0,
+      }
+    ],
+    shipping: {
+      name: "string",
+      email: "string",
+      document: "string",
+    },
+    delivery: {
+      email: "string",
+    },
+    payment: {
+      card: {
+        active: true,
+        owner: {
+          name: "string",
+          document: "string",
+        },
+        name: "string",
+        number: "string",
+        expires: {
+          month: 12,
+          year: 1234,
+        },
+        code: 123,
+      },
+      installments: 1,
+    },
+  };
   await runMiddleware(req, res, cors)
-  res.status(201)
+
+  if (req.method === 'GET') {
+    res.status(200).json({
+      payload
+    })
+  } else if (req.method === 'POST') {
+    if (validate(req.body, res)) {
+      res.status(201).json({
+        orderId: `#100${randomId}`
+      })
+    } else {
+      res.status(400).json({
+        message: 'Reveja os dados enviados'
+      })
+    }
+  } else {
+    res.status(400).json({
+      message: 'Método não implementado'
+    })
+  }
 }
